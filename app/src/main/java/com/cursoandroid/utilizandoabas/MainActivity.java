aeb289fb86534863cb14f1192a436e3ab491509a
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2022-02-17 14:57:34 -0300

    utilizandoabas

---
+++ app/src/main/java/com/cursoandroid/utilizandoabas/MainActivity.java
@@ -0,0 +1,41 @@
+package com.cursoandroid.utilizandoabas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.cursoandroid.utilizandoabas.Fragment.EmAltaFragment;
+import com.cursoandroid.utilizandoabas.Fragment.HomeFragment;
+import com.cursoandroid.utilizandoabas.Fragment.InscricoesFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+        getSupportActionBar().setElevation(0);
+
+        //configurar adapter para as abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Home", HomeFragment.class)
+                        .add("Inscrições", InscricoesFragment.class)
+                        .add("Em Alta", EmAltaFragment.class)
+                .create()
+        );
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
